d185989efda378823652c23b56695dfd4ff41c8f
Alex Hewson <69345308+alexhewson@users.noreply.github.com>
2024-12-12 11:24:07 +0000

    layout updates

--- src/visualization/sentencing/plotly_sentence_lengths.py
+++ src/visualization/sentencing/plotly_sentence_lengths.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # Importing libraries
 import os
 
@@ -13,21 +10,30 @@ from dotenv import find_dotenv, load_dotenv
 
 from src.visualization import prt_theme
 
-##Loading environment variables
+## Loading environment variables
 dotenv_path = find_dotenv()
 load_dotenv(dotenv_path)
 
-##Adding plotly credentials
+## Adding Plotly credentials
 chart_studio.tools.set_credentials_file(
     username=os.getenv("PLOTLY_USERNAME"), api_key=os.getenv("PLOTLY_API_KEY")
 )
-#Setting default Plotly template
+# Setting default Plotly template
 pio.templates.default = "prt_template"
 
-#Read in datasets
+# Read in datasets
 df = pd.read_csv("data/processed/sentencing/sentence_lengths.csv")
-#Filtering for every other year
+# Filtering for every other year
 df = df[::2]
+
+# Create text arrays with labels only for the first and last observations
+text_total = [""] * len(df)
+text_indictable = [""] * len(df)
+if len(df) > 1:
+    text_total[0] = f"{df['total'].iloc[0]} months"
+    text_total[-1] = f"{df['total'].iloc[-1]} months"
+    text_indictable[0] = f"{df['indictable'].iloc[0]} months"
+    text_indictable[-1] = f"{df['indictable'].iloc[-1]} months"
 
 ## Plotting
 fig = go.Figure()
@@ -36,11 +42,12 @@ fig.add_trace(
         x=df["total"],
         y=df["year"],
         orientation="h",
-        # hovertemplate="%{text:,.0f} incidents<extra></extra>",
-        text = df['total'],
-        texttemplate="%{x} months",
+        name="All offences",
+        text=text_total,
+        texttemplate="%{text}",
         textposition="inside",
-        zorder=1 #setting trace to be on top
+        hovertemplate="<b>%{y}</b>: %{x} months",
+        zorder=1,  # Setting trace to be on top
     ),
 )
 fig.add_trace(
@@ -48,29 +55,33 @@ fig.add_trace(
         x=df["indictable"],
         y=df["year"],
         orientation="h",
-        # hovertemplate="%{text:,.0f} incidents<extra></extra>",
-        text = df['indictable'],
-        texttemplate="%{x} months",
+        name="Indictable offences (more serious)",
+        text=text_indictable,
+        texttemplate="%{text}",
         textposition="inside",
-        zorder=0 #setting trace to be overlaid
+        hovertemplate="<b>%{y}</b>: %{x} months",
+        zorder=-1,  # Setting trace to be overlaid
     ),
 )
 
 fig.update_yaxes(autorange="reversed")
+fig.update_xaxes(zeroline=False)
 
 fig.update_layout(
-    barmode='overlay',
+    barmode="overlay",
+    hovermode='closest',
     xaxis_ticks="inside",
-    # xaxis_tickformat= ",.0f",
-    margin_pad = 5,
-    margin = dict(t=20, b=25, l=40, r=25),
-    )
+    margin_pad=5,
+    margin=dict(t=20, b=25, l=40, r=25),
+)
 
 ## Chart annotations
 annotations = []
 
 # Add y-axis label annotation with placement based on dataframe column
-prt_theme.add_annotation(annotations, "Average sentence length", annotation_type="y-axis")
+prt_theme.add_annotation(
+    annotations, "Average sentence length", annotation_type="y-axis"
+)
 
 # Adding annotations to layout
 fig.update_layout(annotations=annotations)
