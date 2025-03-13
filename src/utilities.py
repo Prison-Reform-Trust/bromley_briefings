a20ea3c77d35de4dccad2a1f79aab03dec9eea3a
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-03-13 12:47:02 +0000

    refactor: enhance load_data function to support optional column selection

--- src/utilities.py
+++ src/utilities.py
@@ -30,9 +30,9 @@ def setup_plotly_credentials():
     pio.templates.default = "prt_template"
 
 ## Read data
-def load_data(filepath:str) -> pd.DataFrame:
-    """Loads processed data from CSV."""
-    return pd.read_csv(filepath)
+def load_data(filepath: str, usecols=None) -> pd.DataFrame:
+    """Loads processed data from CSV with optional column selection."""
+    return pd.read_csv(filepath, usecols=usecols)
 
 ## Generate annotations dynamically
 def generate_annotations(traces, colorway, max_chars=None, y_label=None, y_label_placement=None, y_offset_dict=None, x_pad=None):
