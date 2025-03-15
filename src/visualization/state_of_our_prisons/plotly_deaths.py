2e6f39e21f9e3ec756e83fae8c7f8aab07067a66
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-03-15 18:19:14 +0000

    Refactor deaths visualization script to improve structure and modularity

--- src/visualization/state_of_our_prisons/plotly_deaths.py
+++ src/visualization/state_of_our_prisons/plotly_deaths.py
@@ -1,67 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Importing libraries
-import os
+"""
+Title: Remand on the rise
+Subtitle: The number of people in prison on remand is now at its highest level in at least 50 years
+Source: Ministry of Justice (2024). Offender management statistics quarterly: January to March 2024. And previous editions
+"""
 
-import chart_studio
+import os
 import chart_studio.plotly as py
 import pandas as pd
 import plotly.graph_objs as go
-import plotly.io as pio
-from plotly.subplots import make_subplots
-from dotenv import find_dotenv, load_dotenv
 
-from src.visualization import prt_theme
+# Local modules
+import src.utilities as utils
+import src.visualization.prt_theme as prt_theme
 
-##Loading environment variables
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
+# Load configuration
+config = utils.read_config()
 
-##Adding plotly credentials
-chart_studio.tools.set_credentials_file(
-    username=os.getenv("PLOTLY_USERNAME"), api_key=os.getenv("PLOTLY_API_KEY")
-)
-#Setting default Plotly template and assigning attributes to prt_template
-pio.templates.default = "prt_template"
-prt_template = prt_theme.pio.templates['prt_template']
+def create_chart(df: pd.DataFrame) -> go.Figure:
+    """Creates a sunburst chart showing number and proportion of deaths in prison by type and gender."""
 
-#Read in datasets
-df = pd.read_csv("data/processed/safety/deaths.csv", dtype={'parent': 'str'})
+    fig = go.Figure()
 
-## Plotting
+    fig.add_trace(go.Sunburst(
+        ids=df["ids"],
+        labels=df['death_type'],
+        parents=df["parent"],
+        values=df["value"],
+        branchvalues="total",
+        texttemplate="<b>%{label}</b><br>%{value}",
+        hovertemplate="<b>%{label}</b><br>%{value} deaths<br>%{percentParent: .0%} of %{parent}<extra></extra>",
+        marker_line_color="#F7F2F2",
+        insidetextorientation="horizontal",
+        ))
 
-fig = go.Figure()
+    fig.update_layout(
+        margin=dict(t=0, b=0, l=0, r=0),
+        )
+    
+    return fig
 
-fig.add_trace(go.Sunburst(
-    ids=df["ids"],
-    labels=df['death_type'],
-    parents=df["parent"],
-    values=df["value"],
-    branchvalues="total",
-    texttemplate="<b>%{label}</b><br>%{value}",
-    hovertemplate="<b>%{label}</b><br>%{value} deaths<br>%{percentParent: .0%} of %{parent}<extra></extra>",
-    marker_line_color="#F7F2F2",
-    insidetextorientation="horizontal",
-    ))
+def main() -> go.Figure:
+    """Loads data, generates the chart, and uploads it to Chart Studio."""
+    utils.setup_plotly_credentials()
+    data_path = os.path.join(config['data']['clnFilePath'], "state_of_our_prisons/deaths.csv")
+    df = utils.load_data(data_path)
+    fig = create_chart(df)
+    py.plot(fig, filename="deaths")
+    return fig
 
-fig.update_layout(
-    margin = dict(t=20, b=20),
-    uniformtext_minsize=10, 
-    uniformtext_mode='hide',
-    )
 
-## Chart annotations
-annotations = []
-
-# Add title
-# prt_theme.add_title(fig, "Nearly 300 people died in prison in the year to March 2024", width=65)
-
-# Add source annotation
-# prt_theme.add_annotation(annotations, "Table 2, Ministry of Justice (2024). Safety in custody:<br>Quarterly update to December 2023.", annotation_type="source", y=-0.03)
-
-# Adding annotations to layout
-fig.update_layout(annotations=annotations)
-
-# fig.show()
-py.plot(fig, filename="deaths")
+if __name__ == "__main__":
+    main()
