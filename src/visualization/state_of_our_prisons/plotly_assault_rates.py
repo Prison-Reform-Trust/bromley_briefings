0609e9e10271eba89355f8ff157f7dbee9c56942
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-03-16 18:20:46 +0000

    Update assaults data for 2023 and refactor visualization script for improved modularity and clarity

--- src/visualization/state_of_our_prisons/plotly_assault_rates.py
+++ src/visualization/state_of_our_prisons/plotly_assault_rates.py
@@ -1,109 +1,112 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Importing libraries
+"""
+Title: Assaults in prisons in England and Wales
+Subtitle: Assaults and serious assaults declined during the pandemic—but are rising again
+Source: Ministry of Justice (2024). Safety in custody: quarterly update to December 2023.
+"""
+
 import os
 
-import chart_studio
-import chart_studio.plotly as py
 import pandas as pd
+import chart_studio.plotly as py
 import plotly.graph_objs as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
-from dotenv import find_dotenv, load_dotenv
 
-from src.visualization import prt_theme
+# Local modules
+import src.utilities as utils
+import src.visualization.prt_theme as prt_theme
 
-##Loading environment variables
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
+# Load configuration
+config = utils.read_config()
 
-##Adding plotly credentials
-chart_studio.tools.set_credentials_file(
-    username=os.getenv("PLOTLY_USERNAME"), api_key=os.getenv("PLOTLY_API_KEY")
-)
-#Setting default Plotly template and assigning attributes to prt_template
-pio.templates.default = "prt_template"
-prt_template = prt_theme.pio.templates['prt_template']
+def generate_traces(df:pd.DataFrame) -> go.Figure:
+    """Generates Plotly subplot figure and traces for assaults and serious assault"""
+    
+    fig = make_subplots(specs=[[{"secondary_y": True}]])
+    
+    fig.add_trace(
+        go.Scatter(
+            x=df["year"],
+            y=df["serious"],
+            mode="lines+markers",
+            name="serious assault rate",
+            hovertemplate="%{y} serious assaults per 1,000 prisoners<extra></extra>",
+        ),
+        secondary_y=True,
+    )
 
-#Read in datasets
-df = pd.read_csv("data/processed/safety/assaults.csv")
+    fig.add_trace(
+        go.Scatter(
+            x=df["year"],
+            y=df["assaults"],
+            mode="lines+markers",
+            name="assault rate",
+            hovertemplate="%{y} assaults per 1,000 prisoners<extra></extra>",
+        ),
+        secondary_y=False,
+    )
+    return fig
 
-## Plotting
-# Create figure with secondary y-axis
-fig = make_subplots(specs=[[{"secondary_y": True}]])
+def create_chart(df: pd.DataFrame) -> go.Figure:
+    """Creates a Plotly line chart of assault and serious assault rates in prison since 2012."""
+    
+    fig = generate_traces(df)
+    colorway = pio.templates[pio.templates.default].layout.colorway
+    
+    annotations = prt_theme.add_annotation(None, "Incidents per 1,000 prisoners", annotation_type="y-axis", y=1)
+    prt_theme.add_annotation(annotations, "The definition of recorded<br>assaults changed in 2019", annotation_type="label", xref="x", yref="y", x=2019.5, y=475)
 
-fig.add_trace(
-    go.Scatter(
-        x=df["year"],
-        y=df["serious"],
-        mode="lines+markers",
-        name="serious assault rate",
-        hovertemplate="%{y} serious assaults per 1,000 prisoners<extra></extra>",
-    ),
-    secondary_y=True,
-)
+    # Configure axes
+    fig.update_yaxes(
+        title_text="Serious assaults",
+        range=[0,51],
+        titlefont_color=colorway[0],
+        tickfont_color=colorway[0],
+        automargin=True,
+        overlaying="y",
+        tickmode="sync",
+        secondary_y=True)
 
-fig.add_trace(
-    go.Scatter(
-        x=df["year"],
-        y=df["assaults"],
-        mode="lines+markers",
-        name="assault rate",
-        hovertemplate="%{y} assaults per 1,000 prisoners<extra></extra>",
-    ),
-    secondary_y=False,
-)
+    fig.update_yaxes(
+        title_text="Assaults", 
+        range=[0,510],
+        title_standoff=20,
+        titlefont_color=colorway[1],
+        tickfont_color=colorway[1],
+        automargin=True,
+        secondary_y=False)
+    
+    # fig.update_xaxes(autorangeoptions_maxallowed=2024) Autorange works locally but doesn't on Chart Studio
 
-# Set y-axes titles
-fig.update_yaxes(
-    title_text="Serious assaults",
-    range=[0,51],
-    titlefont_color=prt_template.layout.colorway[0],
-    tickfont_color=prt_template.layout.colorway[0],
-    overlaying="y",
-    tickmode="sync",
-    secondary_y=True)
+    # Configure layout
+    fig.update_layout(
+        annotations=annotations)
 
-fig.update_yaxes(
-    title_text="Assaults", 
-    range=[0,510],
-    titlefont_color=prt_template.layout.colorway[1],
-    tickfont_color=prt_template.layout.colorway[1],
-    secondary_y=False)
+    ## Adding dotted line for recording change
+    fig.add_shape(
+        type="line",
+        x0=2019,
+        y0=0,
+        x1=2019,
+        y1=500,
+        line=dict(color=pio.templates[pio.templates.default].layout.xaxis.tickcolor, width=1, dash="dot"),
+        layer="below",
+    )
+    return fig
+    
 
-# Axis parameter adjustments
-fig.update_layout(
-    xaxis_ticks="inside",
-    margin_pad = 5)
+def main() -> go.Figure:
+    """Loads data, generates the chart, and uploads it to Chart Studio."""
+    utils.setup_plotly_credentials()
+    data_path = os.path.join(config['data']['clnFilePath'], "state_of_our_prisons/assaults.csv")
+    df = utils.load_data(data_path)
+    fig = create_chart(df)
+    py.plot(fig, filename="assault_rates")
+    return fig
 
-## Adding dotted line for recording change
-fig.add_shape(
-    type="line",
-    x0=2019,
-    y0=0,
-    x1=2019,
-    y1=500,
-    line=dict(color=prt_template.layout.xaxis.tickcolor, width=1, dash="dot"),
-    layer="below",
-)
-## Chart annotations
-annotations = []
 
-# Add title
-prt_theme.add_title(fig, "Assaults and serious assaults declined during the pandemic—but are rising again")
-
-# Add source annotation with default placement
-prt_theme.add_annotation(annotations, "Table 3.1, Ministry of Justice (2024). Safety in custody:<br>Quarterly update to December 2023.", annotation_type="source")
-
-# Add y-axis label annotation with placement based on dataframe column
-prt_theme.add_annotation(annotations, "Incidents per 1,000 prisoners", annotation_type="y-axis", y=1.05)
-
-# Add note
-prt_theme.add_annotation(annotations, "The definition of recorded<br>assaults changed in 2019", xref="x", yref="y", x=2019.5, y=475)
-
-# Adding annotations to layout
-fig.update_layout(annotations=annotations)
-
-# fig.show()
-py.plot(fig, filename="assault_rates")
+if __name__ == "__main__":
+    main()
