bd8179be0a31aebfb332ba84b4813fe27b7f94e8
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-03-28 11:19:33 +0000

    linting

--- src/visualization/state_of_our_prisons/plotly_private_prisons.py
+++ src/visualization/state_of_our_prisons/plotly_private_prisons.py
@@ -4,24 +4,23 @@
 """
 Title: Private prisons in England and Wales
 Subtitle:
-Source: 
+Source:
     - Beard, J. (2023). The prison estate in England and Wales. House of Commons.
     - HM Prison and Probation Service. (2025).  Prisons and their resettlement providers.
 """
 
 import os
+
 import chart_studio.plotly as py
 import pandas as pd
 import plotly.graph_objs as go
-import plotly.io as pio
-from dotenv import find_dotenv, load_dotenv
 
 # Local modules
 import src.utilities as utils
-import src.visualization.prt_theme as prt_theme
 
 # Load configuration
 config = utils.read_config()
+
 
 def generate_traces(df: pd.DataFrame) -> list:
     """Generate traces for the chart."""
@@ -34,14 +33,14 @@ def generate_traces(df: pd.DataFrame) -> list:
 
     traces = [
         go.Scattermap(
-            lat = group['latitude'],
-            lon = group['longitude'],
-            mode = 'markers',
+            lat=group['latitude'].tolist(),
+            lon=group['longitude'].tolist(),
+            mode='markers',
             marker=go.scattermap.Marker(
                 size=9,
-                color= colors[provider],
+                color=colors[provider],
             ),
-            text = group['prison_name'],
+            text=group['prison_name'].tolist(),
             hovertemplate="%{text}",
             name=str(provider),
         )
@@ -49,6 +48,7 @@ def generate_traces(df: pd.DataFrame) -> list:
     ]
 
     return traces
+
 
 def create_chart(df: pd.DataFrame) -> go.Figure:
 
@@ -82,22 +82,32 @@ def create_chart(df: pd.DataFrame) -> go.Figure:
             zoom=5.3,
         ),
     )
-    
     return fig
+
 
 def generate_html(fig: go.Figure) -> None:
     """Saves the HTML content to a file."""
-    output_path = os.path.join(config['viz']['outPath'], "state_of_our_prisons/private_prisons.html")
-    fig_html = fig.write_html(output_path, full_html=True, include_plotlyjs="cdn", config = {'displayModeBar': False})
+    output_path = os.path.join(
+        config['viz']['outPath'],
+        "state_of_our_prisons/private_prisons.html")
+    fig_html = fig.write_html(
+        output_path,
+        full_html=True,
+        include_plotlyjs="cdn",
+        config={'displayModeBar': False})
     return fig_html
+
 
 def main() -> go.Figure:
     """Loads data, generates the chart, and uploads it to Chart Studio."""
     utils.setup_plotly_credentials()
-    data_path = os.path.join(config['data']['clnFilePath'], "state_of_our_prisons/private_prisons.csv")
+    data_path = os.path.join(
+        config['data']['clnFilePath'],
+        "state_of_our_prisons/private_prisons.csv")
     df = utils.load_data(data_path)
     fig = create_chart(df)
-    generate_html(fig)
+    # generate_html(fig)
+    py.plot(fig, filename="private_prisons")
     return fig
 
 
