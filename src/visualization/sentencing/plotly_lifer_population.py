3f845500c1d2b3528f375c6651bc49c22396cce2
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2025-11-11 16:46:55 +0000

    Refactor Plotly chart generation: update HTML output and improve code structure

--- src/visualization/sentencing/plotly_lifer_population.py
+++ src/visualization/sentencing/plotly_lifer_population.py
@@ -2,14 +2,12 @@
 # -*- coding: utf-8 -*-
 
 """
-Title: Life behind bars
-Subtitle: The number of people in prison serving a life sentence has almost trebled in the last 30 years
-Source: Ministry of Justice (2024). Offender management statistics quarterly: January to March 2024. And previous editions
+A Plotly chart showing the growth of life sentences in England & Wales.
+The chart is saved as an HTML file using a Jinja2 template for embedding in a web page.
 """
 
 import os
 
-import chart_studio.plotly as py
 import pandas as pd
 import plotly.graph_objs as go
 
@@ -18,14 +16,28 @@ import src.utilities as utils
 import src.visualization.prt_theme as prt_theme
 
 # Load configuration
-config = utils.read_config()
+CONFIG = utils.read_config()
+
+# Jinja2 template variables
+TITLE = "Life behind bars"
+SUBTITLE = "The number of people in prison serving a life sentence has almost trebled in the last 30 years"
+SOURCE = "Ministry of Justice (2024). Offender management statistics quarterly: April to June 2024."
+OUTPUT_PATH = utils.get_output_path(
+    section='sentencing',
+    filename='lifer_population.html'
+)
+
 
 def create_chart(df: pd.DataFrame) -> go.Figure:
     """Creates a line chart showing change in number of people in prison serving a life sentence."""
 
     fig = go.Figure()
-    annotations = prt_theme.add_annotation(None, "People in prison", annotation_type="y-axis")
-    
+    annotations = prt_theme.add_annotation(
+        annotations_list=None,
+        text="People in prison",
+        annotation_type="y-axis"
+    )
+
     fig.add_trace(
         go.Scatter(
             name="Lifer population",
@@ -36,7 +48,7 @@ def create_chart(df: pd.DataFrame) -> go.Figure:
         ),
     )
     # Configure axes
-    fig.update_yaxes(range=[0, 10050],automargin=True, tickformat= ",.0f")
+    fig.update_yaxes(range=[0, 10050], automargin=True, tickformat=",.0f")
     fig.update_xaxes(dtick=5)
 
     # Configure layout
@@ -46,13 +58,26 @@ def create_chart(df: pd.DataFrame) -> go.Figure:
         )
     return fig
 
-def main() -> go.Figure:
-    """Loads data, generates the chart, and uploads it to Chart Studio."""
+
+def prepare_chart() -> go.Figure:
+    """Loads data and prepares the Plotly chart."""
     utils.setup_plotly_template()
-    data_path = os.path.join(config['data']['clnFilePath'], "sentencing/lifer_population.csv")
+    data_path = os.path.join(CONFIG['data']['clnFilePath'], "sentencing/lifer_population.csv")
     df = utils.load_data(data_path)
     fig = create_chart(df)
-    py.plot(fig, filename="lifer_population")
+    return fig
+
+
+def main() -> go.Figure:
+    """Generates the chart, and saves it as an HTML file using a Jinja2 template."""
+    fig = prepare_chart()
+    utils.save_plotly_chart_as_html(
+        fig=fig,
+        output_path=OUTPUT_PATH,
+        title=TITLE,
+        subtitle=SUBTITLE,
+        source=SOURCE
+    )
     return fig
 
 
