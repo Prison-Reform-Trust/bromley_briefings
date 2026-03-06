c5afa770f7f51e77d058ea52e1af9237cd177e37
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2026-03-06 16:38:51 +0000

    Added new HTML output

--- src/visualization/state_of_our_prisons/plotly_private_prisons.py
+++ src/visualization/state_of_our_prisons/plotly_private_prisons.py
@@ -2,27 +2,33 @@
 # -*- coding: utf-8 -*-
 
 """
-Title: Private prisons in England and Wales
-Subtitle:
-Source: 
-    - Beard, J. (2023). The prison estate in England and Wales. House of Commons.
-    - HM Prison and Probation Service. (2025).  Prisons and their resettlement providers.
+A Plotly map showing all private prisons in England and Wales.
+The chart is saved as an HTML file using a Jinja2 template for embedding in a web page.
 """
 
 import os
 
-import chart_studio.plotly as py
 import pandas as pd
 import plotly.graph_objs as go
-import plotly.io as pio
-from dotenv import find_dotenv, load_dotenv
 
 # Local modules
 import src.utilities as utils
-import src.visualization.prt_theme as prt_theme
 
 # Load configuration
-config = utils.read_config()
+CONFIG = utils.read_config()
+
+# Jinja2 template variables
+TITLE = "Private prisons in England and Wales"
+SUBTITLE = ""
+SOURCE = (
+    "Beard, J. (2023). The prison estate in England and Wales. House of Commons.<br>"
+    "HM Prison and Probation Service. (2025). Prisons and their resettlement providers."
+)
+OUTPUT_PATH = utils.get_output_path(
+    section='state_of_our_prisons',
+    filename='private_prisons.html'
+)
+
 
 def generate_traces(df: pd.DataFrame) -> list:
     """Generate traces for the chart."""
@@ -35,14 +41,14 @@ def generate_traces(df: pd.DataFrame) -> list:
 
     traces = [
         go.Scattermap(
-            lat = group['latitude'],
-            lon = group['longitude'],
-            mode = 'markers',
+            lat=group['latitude'],
+            lon=group['longitude'],
+            mode='markers',
             marker=go.scattermap.Marker(
                 size=9,
-                color= colors[provider],
+                color=colors[provider],
             ),
-            text = group['prison_name'],
+            text=group['prison_name'],
             hovertemplate="%{text}",
             name=str(provider),
         )
@@ -51,14 +57,16 @@ def generate_traces(df: pd.DataFrame) -> list:
 
     return traces
 
-def create_chart(df: pd.DataFrame) -> go.Figure:
 
-    fig = go.Figure()
+def create_chart(df: pd.DataFrame) -> go.Figure:
+    """Creates a Plotly map of all private prisons in England and Wales."""
+    fig = go.Figure()  # TODO: #45 Fix rendering of the Maplibre attribution below maps
     traces = generate_traces(df)
     fig.add_traces(traces)
 
     fig.update_layout(
-        autosize=True,
+        # autosize=True,
+        height=500,
         hovermode='closest',
         margin_l=0,
         margin_r=0,
@@ -83,23 +91,29 @@ def create_chart(df: pd.DataFrame) -> go.Figure:
             zoom=5.3,
         ),
     )
-    
+
     return fig
 
-def generate_html(fig: go.Figure) -> None:
-    """Saves the HTML content to a file."""
-    output_path = os.path.join(config['viz']['outPath'], "state_of_our_prisons/private_prisons.html")
-    fig_html = fig.write_html(output_path, full_html=True, include_plotlyjs="cdn", config = {'displayModeBar': False})
-    return fig_html
 
-def main() -> go.Figure:
-    """Loads data, generates the chart, and uploads it to Chart Studio."""
+def prepare_chart() -> go.Figure:
+    """Loads data and prepares the Plotly chart."""
     utils.setup_plotly_template()
-    data_path = os.path.join(config['data']['clnFilePath'], "state_of_our_prisons/private_prisons.csv")
+    data_path = os.path.join(CONFIG['data']['clnFilePath'], "state_of_our_prisons/private_prisons.csv")
     df = utils.load_data(data_path)
     fig = create_chart(df)
-    generate_html(fig)
     return fig
+
+
+def main() -> None:
+    """Generates the chart, and saves it as an HTML file using a Jinja2 template."""
+    fig = prepare_chart()
+    utils.save_plotly_chart_as_html(
+        fig=fig,
+        output_path=OUTPUT_PATH,
+        title=TITLE,
+        subtitle=SUBTITLE,
+        source=SOURCE,
+    )
 
 
 if __name__ == "__main__":
