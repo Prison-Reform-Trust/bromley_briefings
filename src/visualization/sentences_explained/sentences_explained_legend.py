db2596b0823218537122ac893b9e3235c383d588
Alex Hewson <alex.hewson@prisonreformtrust.org.uk>
2026-04-12 12:53:08 +0100

    WIP

--- src/visualization/sentences_explained/sentences_explained_legend.py
+++ src/visualization/sentences_explained/sentences_explained_legend.py
@@ -11,7 +11,6 @@ import os
 
 import pandas as pd
 import plotly.graph_objs as go
-import plotly.io as pio
 
 # Local modules
 import src.utilities as utils
@@ -21,7 +20,7 @@ from src.visualization import prt_theme
 CONFIG = utils.read_config()
 
 # Jinja2 template variables
-TITLE = ""
+TITLE = "Legend"
 SUBTITLE = ""
 SOURCE = ""
 OUTPUT_PATH = utils.get_output_path(
@@ -31,57 +30,46 @@ OUTPUT_PATH = utils.get_output_path(
 
 
 def prepare_data(df: pd.DataFrame) -> pd.DataFrame:
-    """Transforms data from wide to long"""
-    df = df.T.reset_index()
-    df.columns = df.iloc[0, :]
-    df.drop(index=df.index[0], axis=0, inplace=True)
-    return df
-
-
-def generate_labels(df: pd.DataFrame) -> tuple[list, list]:
-    """Generates labels for all offences and indictable offences"""
-    text_total = [""] * len(df)
-    text_indictable = [""] * len(df)
-
-    if len(df) > 1:
-        for idx in [0, -4, -1]:
-            if idx < len(df):
-                text_total[idx] = f"{df['total'].iloc[idx]} months"
-                text_indictable[idx] = f"{df['indictable'].iloc[idx]} months"
-
-    return text_total, text_indictable
+    """Transforms data from wide to long format by transposing,
+    setting the first row as column headers, and resetting the index."""
+    return (
+        df.T.reset_index()  # Convert index to a column (preserves sentence values)
+        .pipe(lambda x: x.set_axis(x.iloc[0], axis=1))  # Set columns to the first row
+        .iloc[1:]  # Drop the first row (now used as headers)
+        .reset_index(drop=True)  # Reset the row index
+    )
 
 
 def generate_traces(df: pd.DataFrame) -> list:
     """Generates Plotly traces"""
-    # text_total, text_indictable = generate_labels(df)
     colorway = ("#F9A237", "#A01D28", "#808080", "#499CC9", "#573D6B")
 
     traces = [
         go.Bar(
-            x=df['value'],
-            y=df['sentence'],
+            x=[df['value'][i]],
+            y=[1],
             orientation="h",
-            text=df['text'].title(),
+            text=prt_theme.wrap_labels(df['text'][i].capitalize(), max_chars=17),
             textposition="inside",
+            insidetextanchor="middle",
+            textfont_color="white",
+            textfont_size=14,
+            customdata=[prt_theme.wrap_labels(df['text'][i].capitalize(), max_chars=25)],  # Using %{text} in hovertemplate didn't render correctly, this is a workaround
+            hovertemplate="%{customdata}<extra></extra>",
             marker_color=colorway[i],
+            width=7,
         )
-        for i, sentence in enumerate(df)  # This needs updating to account for new transposition of data
+        for i, sentence in enumerate(df['sentence'])
     ]
 
     return traces
 
 
 def create_chart(df: pd.DataFrame) -> go.Figure:
-    """Creates a Plotly horizontal bar chart of average sentence lengths by year."""
+    """Creates a Plotly horizontal bar chart."""
 
     fig = go.Figure()
     traces = generate_traces(df)
-    # annotations = prt_theme.add_annotation(
-    #     annotations_list=None,
-    #     text="Average sentence length",
-    #     annotation_type="y-axis"
-    # )
 
     fig.add_traces(traces)
 
@@ -94,11 +82,13 @@ def create_chart(df: pd.DataFrame) -> go.Figure:
     # Configure layout
     fig.update_layout(
         height=100,
+        margin={'t': 0, 'b': 0, 'l': 0, 'r': 0},
+        uniformtext_minsize=9,
+        uniformtext_mode='show',
         barmode="stack",
         hovermode="closest",
         yaxis_showticklabels=False,
         xaxis_showticklabels=False,
-        # annotations=annotations,
     )
     return fig
 
@@ -113,7 +103,7 @@ def load_data() -> pd.DataFrame:
 def prepare_chart() -> go.Figure:
     """Loads data and prepares the Plotly chart."""
     utils.setup_plotly_template()
-    df = load_data()
+    df = load_data().pipe(prepare_data)
     fig = create_chart(df)
     return fig
 
